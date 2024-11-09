24864d3f5e517afcba9aa1a337b9f6f248181d5d
kyjohnso <kyjohnso@gmail.com>
2024-11-09 18:47:47 +0000

    change initial camera angle

--- ideal_gas/app.js
+++ ideal_gas/app.js
@@ -2,6 +2,8 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 40;
+camera.position.y = 25;
+camera.position.x = 25;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
